0104597c94202884ab583cf36bea3d1e83708bca
heyjulieh <heyjulieh@gmail.com>
2017-05-30 20:34:59 -0700

    added bootstrap and updated routes per Jane

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'index#index'
-  resources :users
+
   resources :tips
+
+  get '/users/:user_id/tips', to: 'usertips#index', as: 'user_tips'
+  get '/users/:user_id/tips/new', to: 'usertips#new', as: 'new_user_tip'
+  post '/users/:user_id/tips', to: 'usertips#create'
+  get '/users/:user_id/tips/:id', to: 'usertips#show', as: 'user_tip'
+  get '/users/:user_id/tips/:id/edit', to: 'usertips#edit', as: 'edit_user_tip'
+  patch '/users/:user_id/tips/:id', to: 'usertips#update'
+  delete '/users/:user_id/tips/:id', to: 'usertips#destroy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
