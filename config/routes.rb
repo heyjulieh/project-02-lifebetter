23eb33a690a4acab75df1494d381398cf3d43dd7
janeosaur <janewie@gmail.com>
2017-05-31 12:09:34 -0700

    fix model errors

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/users/', to: 'users#index', as: 'users'
   get '/users/:user_id', to: 'users#show', as: 'user'
 
-  get '/users/:user_id/tips', to: 'usertips#index', as: 'user_tips'
+  # get '/users/:user_id/tips', to: 'usertips#index', as: 'user_tips'
   get '/users/:user_id/tips/new', to: 'usertips#new', as: 'new_user_tip'
   post '/users/:user_id/tips', to: 'usertips#create', as: 'create_user_tip'
   get '/users/:user_id/tips/:id', to: 'usertips#show', as: 'user_tip'
