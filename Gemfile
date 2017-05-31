a6035960661f95765abefcaaa9d7a708f260e2e3
janeosaur <janewie@gmail.com>
2017-05-30 20:48:48 -0700

    users index and show page created

--- Gemfile
+++ Gemfile
@@ -56,7 +56,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 
-  # added new gem
+  # better errors gem
   gem "better_errors"
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  #get '/users/', to: 'users#index', as: 'users'
+  def index
+    @users = User.all
+  end
+
+  #get '/users/:user_id', to: 'users#show', as: 'user'
+  def show
+    @user = User.find (params[:user_id])
+  end
+
 end
