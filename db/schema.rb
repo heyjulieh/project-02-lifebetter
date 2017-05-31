fd59e5489b71ed302a0eb50e6c916a9e1aca0fcc
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-30 17:13:27 -0700

    tips table added joined to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530234837) do
+ActiveRecord::Schema.define(version: 20170531001151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tips", force: :cascade do |t|
+    t.string "genre"
+    t.string "title"
+    t.boolean "important"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tips_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,4 +47,5 @@ ActiveRecord::Schema.define(version: 20170530234837) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tips", "users"
 end
