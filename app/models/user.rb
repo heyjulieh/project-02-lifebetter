d79861205c6b966c23dba46df482e32e1f04de8f
janeosaur <janewie@gmail.com>
2017-05-31 22:03:17 -0700

    users sign in with username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :tips
+  has_many :tips, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-
          :recoverable, :rememberable, :trackable, :validatable#, :lockable ,:confirmable
 
+  def email_required?
+   false
+  end
 
+  def email_changed?
+   false
+  end
 end
