4f7f704e0d7a44ec085c9a4fb4bc6a670bce191c
MJ-sfo <michaeljlaird@yahoo.com>
2017-06-02 15:22:29 -0700

    have birthday field as date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601220858) do
+ActiveRecord::Schema.define(version: 20170602212038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,13 +55,6 @@ ActiveRecord::Schema.define(version: 20170601220858) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "name"
-    t.string "confirmation_token"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
@@ -74,6 +67,7 @@ ActiveRecord::Schema.define(version: 20170601220858) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string "slug"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
